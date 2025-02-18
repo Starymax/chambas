a8d4b8d3fb9f9cfb70de623ee90c959bd538f878
Starymax <llamacosmica61@gmail.com>
2025-02-18 14:45:50 -0600

    Actualización automática

--- c++/2SEMESTRE/FACTORIAL_RECURSIVA.cpp
+++ c++/2SEMESTRE/FACTORIAL_RECURSIVA.cpp
@@ -12,25 +12,41 @@ void serie2(int n){
         cout << n << " ";
     else{
         cout << n << " ";
-        serie2(n + 1)
+        serie2(n + 1);
     }
 }
 
 void serie3(int n, int i = 1){
-    if(i > n)
-        cout << i;
-    else{
+    if(i <= n){
         cout << i << " ";
         serie3(n, i + 1);
     }
 }
 
 long factorial (int n){
-        if(n == 0 || N == 1)
+        if(n == 0 || n == 1)
             return 1;
         else
             return n * factorial(n-1);
-    }
-    int main(){
-        serie3(7)
+}
+
+long potencia(int b, int e){
+    if(e == 0)
+        return 1;
+    if(e == 1)
+        return b;
+    else
+        return b * potencia(b, e - 1);
+}
+
+int digitos(long n){
+    if(n < 10)
+        return 1;
+    else
+        return digitos(n/10) + 1;
+}
+
+int main(){
+    cout << potencia(5,3);
+    cout << digitos(564611);
 }
