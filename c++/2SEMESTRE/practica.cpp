26e78e0c61902e42827f82e1b5182ccf2694dec0
Starymax <llamacosmica61@gmail.com>
2025-04-08 00:49:20 -0600

    Actualización automática

--- c++/2SEMESTRE/practica.cpp
+++ c++/2SEMESTRE/practica.cpp
@@ -2,34 +2,38 @@
 using namespace std;
 
 struct Alumno{
-    float calif;
     string nombre;
+    float calif;
     Alumno *sig;
 };
 
 void mover(Alumno *todos, Alumno *&lista1, Alumno *&lista2){
     if(todos !=NULL){
-        if(todos -> calif >= 6 || todos -> calif < 7){
-            Alumno *lista1 = new Alumno;
-            lista1 -> calif = todos -> calif;
-            lista1 -> nombre = todos -> nombre;
-            lista1 -> sig = NULL;
+        if(todos -> calif >= 6 && todos -> calif < 7){
+            Alumno  *nuevo = new Alumno;
+            nuevo->calif = todos->calif;
+            nuevo->nombre = todos->nombre;
+            nuevo->sig = NULL;
+            lista1 = nuevo;
             mover(todos -> sig, lista1 -> sig, lista2);
         } else {
-            if(todos -> calif < 6 || todos -> calif >= 4){
-                Alumno *lista2 = new Alumno;
-                lista2 -> calif = todos -> calif;
-                lista2 -> sig = NULL;
+            if(todos -> calif < 6 && todos -> calif >= 4){
+                Alumno *nuevo = new Alumno;
+                nuevo->calif = todos->calif;
+                nuevo->nombre = todos->nombre;
+                nuevo->sig = NULL;
+                lista2 = nuevo;
                 mover(todos -> sig, lista1, lista2 -> sig);
-            }
+        }
+        mover(todos -> sig, lista1, lista2);
         }
     }
-}   
+}
 
-void imprimir(Nodo *lista) {
+void imprimir(Alumno *lista) {
 	if (lista != NULL) {
 		cout << lista->nombre << ": " << lista->calif << " ";
-		imprimir(lista->siguiente);
+		imprimir(lista->sig);
 	}
 	else
 		cout << endl;
@@ -38,5 +42,29 @@ void imprimir(Nodo *lista) {
 int main(){
     Alumno *lista1 = NULL;
     Alumno *lista2 = NULL;
-    Alumno *todos;
+    Alumno *todos = new Alumno{
+        "Carlos", 4.9, new Alumno{
+        "DavidSeñor", 2.3, new Alumno{
+        "Denis", 8.5, new Alumno{
+        "EmiMorales", 6.3, new Alumno{
+        "Fernando", 6.9, new Alumno{
+        "Guicho", 4.2, new Alumno{
+        "Isabel", 5.1, new Alumno{
+        "Javier", 7.8, new Alumno{
+        "Lucía", 3.5, new Alumno{
+        "Marlen", 5.7, new Alumno{
+        "Oswaldo", 3.8, new Alumno{
+        "Pepo", 6.6, new Alumno{
+        "Roberto", 5.6, new Alumno{
+        "Ulises", 7.5, new Alumno{
+        "Valentin", 6.0, NULL
+    }}}}}}}}}}}}}}};
+
+    mover(todos, lista1, lista2);
+    cout << "Calificaciones entre 6 y 7:" << endl;
+    imprimir(lista1);
+    cout << "Calificaciones entre 4 y 6:" << endl;
+    imprimir(lista2);
+    getchar();
+    return 0;
 }
--- c++/2SEMESTRE/practica2.cpp
+++ c++/2SEMESTRE/practica2.cpp
@@ -1,49 +1,76 @@
-#include <isotream>
+#include <iostream>
 using namespace std;
 
 struct Poli{
     int coeficiente;
     int exponente;
     Poli *sig;
+};
+
+void insertar(Poli *&base, int exponente, int coeficiente, int n){
+    if(base != NULL){
+        if(base -> exponente < exponente && n == 1){
+            Poli *nuevo = new Poli;
+            nuevo->coeficiente = coeficiente;
+            nuevo->exponente = exponente;
+            nuevo->sig = base;
+            base = nuevo;
+        } else if(base -> exponente == exponente){
+            base->coeficiente += coeficiente;
+        } else if(base -> sig -> exponente < exponente){
+            Poli *nuevo = new Poli;
+            nuevo->coeficiente = coeficiente;
+            nuevo->exponente = exponente;
+            nuevo->sig = base->sig;
+            base->sig = nuevo;
+        } else
+            insertar(base->sig, exponente, coeficiente, n+1);
+    }
 }
 
-void insertar(Poli *head, int exponente, int coeficiente){
-    if(head == NULL || head -> exponente < exponente){
-        Poli *nuevo = new Poli;
-        nuevo -> exponente = head -> exponente;
-        if(head -> siguiente == NULL)
-            nuevo -> siguiente = NULL;
-        else
-            nuevo -> siguiente = head -> siguiente;
-        head -> siguiente = nuevo;
-        head -> exponente = exponente;
-        head -> coeficiente = coeficiente;
-    }
-    else{
-        if(head -> exponente < exponente && head -> siguiente == NULL){
-            Poli *nuevo = new Poli;
-            nuevo -> exponente = head -> exponente;
-            nuevo -> siguiente = NULL;
-            head -> siguiente = nuevo;
-            head -> exponente = exponente;
-            head -> coeficiente = coeficiente;
-        }
-        else{
-            if(head -> exponente <= exponente && (head -> siguiente == NULL || head -> siguiente -> exponente > exponente)){
-                Poli *nuevo = new Poli;
-                nuevo -> exponente = exponente;
-                if(head -> siguiente == NULL)
-                    nuevo -> siguiente = NULL;
-                else
-                    nuevo -> siguiente = head -> siguiente;
-                head -> siguiente = nuevo;
-            }
+void imprimir(Poli *base, int n){
+    if(base != NULL){
+        if(base->coeficiente != 0){
+            if(base->coeficiente > 0 && n > 1)
+                cout << "+ " << base->coeficiente;
             else
-                insertarOrdenado(head -> siguiente, exponente);
+                cout << base->coeficiente;
+            if(base->exponente > 1)
+                cout << 'x' << '^' << base->exponente << " ";
+            else if(base->exponente == 1)
+                cout << 'x';
         }
+        imprimir(base->sig, n+1);
+    } else
+        cout << endl;
+}
+
+int evaluar(Poli *base, int x){
+    if(base != NULL){
+        int potencia = 1;
+        for(int i = 0; i < base->exponente; i++){
+            potencia *= x;
+        }
+        int resultado = potencia * base->coeficiente;
+        return resultado + evaluar(base->sig, x);
     }
+    return 0;
 }
 
 int main(){
+    int n = 1;
+    int exp, coef, x;
+    Poli* base = new Poli{3, 4, new Poli{-4, 2, new Poli{11, 0, NULL}}};
+    imprimir(base, n);
+    cout << "Coeficiente: ";
+    cin >> coef;
+    cout << "Exponiente: ";
+    cin >> exp;
+    insertar(base, exp, coef, n);
+    imprimir(base, n);
+    cout << "Inserte valor de X: ";
+    cin >> x;
+    cout << evaluar(base, x) << endl;
+    getchar();
     return 0;
 }
